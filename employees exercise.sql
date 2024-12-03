58d6862cdfe4c74ecadd03b2221234ab067db374
Dharmin2024 <dharmin.polra@gmail.com>
2024-12-03 18:11:27 +0000

    Add files via upload

---
+++ employees exercise.sql
@@ -0,0 +1,15 @@
+-- selecting one column from employees table
+SELECT 
+    lastname
+FROM
+    employees;
+    
+-- selecting multiple columns from employees table
+SELECT 
+    lastname, firstname, jobtitle
+FROM
+    employees;
+    
+-- selecting all employee
+SELECT*
+FROM employees;
